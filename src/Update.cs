7e0c730b38e88b681a44ad0f45ebc6f58fdbd760
jooapa <jooa.akonpelto@gmail.com>
2024-01-21 21:03:02 +0200

    Refactor function to improve performance

---
+++ src/Update.cs
@@ -0,0 +1,34 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace jammer { 
+    public class Update {
+        public static async Task update(string version) {
+            using (HttpClient client = new HttpClient()) {
+                string downloadUrl = "https://github.com/jooapa/jammer/releases/download/X.X.X/jammer-Setup_V"+ version +".exe";
+                string savePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Desktop\\jammer-Setup_V"+ version +".exe";
+                await client.GetAsync(downloadUrl, HttpCompletionOption.ResponseHeadersRead)
+                    .ContinueWith(async (responseTask) => {
+                        var response = await responseTask;
+                        using (var contentStream = await response.Content.ReadAsStreamAsync())
+                        using (var fileStream = new FileStream(savePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+                        {
+                            await contentStream.CopyToAsync(fileStream);
+                        }
+                    });
+            }
+        }
+
+        public static bool checkForUpdate(string version) {
+            string url = "https://raw.githubusercontent.com/jooapa/jammer/master/VERSION";
+            string latestVersion = "";
+            using (HttpClient client = new HttpClient()) {
+                latestVersion = client.GetStringAsync(url).Result;
+            }
+            if (latestVersion != version) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
