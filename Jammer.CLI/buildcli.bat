ac156aec9021ee7c90dc106f00b5d9ef7739d175
jooapas <jooa.akonpelto@gmail.com>
2024-05-24 13:49:01 +0300

    chore: Update version numbers to 2.3.5.6

--- Jammer.CLI/buildcli.bat
+++ Jammer.CLI/buildcli.bat
@@ -21,7 +21,7 @@ COPY /Y LICENSE %targetFolder%
 
 mkdir "%targetFolder%\locales"
 XCOPY /S /Y ..\locales "%targetFolder%\locales"
-SET "start_name=Jammer-Setup_V2.3.4.5.exe"
+SET "start_name=Jammer-Setup_V2.3.5.6.exe"
 
 makensis %targetFolder%\setup.nsi  
 
