595c70fe55d7aa42111c9278e18f80c146d155ce
jooapa <jooa.akonpelto@gmail.com>
2024-05-28 18:39:21 +0300

    chore: Update git clone command and build scripts in usb-appimage.sh

--- usb-appimage.sh
+++ usb-appimage.sh
@@ -1,5 +1,6 @@
 sudo apt install -y git dotnet7 libfuse2
 
+echo "Cloning jammer repository and building it..."
 git clone https://github.com/jooapa/jammer.git ./jammer && \
     dotnet restore && \
     ./build.sh
