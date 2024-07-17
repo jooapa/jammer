a860af34b162f280f67d50b5ceabf0f94529f686
jooapas <jooa.akonpelto@gmail.com>
2024-07-18 01:13:34 +0300

    chore: Update version to 2.11.2.2

--- Jammer.Core/src/Utils.cs
+++ Jammer.Core/src/Utils.cs
@@ -41,7 +41,7 @@ namespace Jammer
         public static bool isDebug = false;
         public static string currentPlaylist = "";
         public static bool isInitialized = false;
-        public static string version = "2.10.1.1";
+        public static string version = "2.11.2.2";
         public static string? AppDirMount = Environment.GetEnvironmentVariable("APPDIR");
         public static float MusicTimePercentage = 0;
     }
