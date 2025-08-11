576b888fe08260aba9b149961ab913843b50ebe5
jooapa <37744313+jooapa@users.noreply.github.com>
2025-08-12 01:45:59 +0300

    add xml namesppaces

--- Jammer.Core/src/Rss.cs
+++ Jammer.Core/src/Rss.cs
@@ -61,6 +61,13 @@ namespace Jammer
                 var xmlDoc = new XmlDocument();
                 xmlDoc.LoadXml(rssContent);
 
+                // Create a namespace manager and add common namespaces
+                var namespaceManager = new XmlNamespaceManager(xmlDoc.NameTable);
+                namespaceManager.AddNamespace("dc", "http://purl.org/dc/elements/1.1/");
+                namespaceManager.AddNamespace("media", "http://search.yahoo.com/mrss/");
+                namespaceManager.AddNamespace("itunes", "http://www.itunes.com/dtds/podcast-1.0.dtd");
+                namespaceManager.AddNamespace("custom", "http://example.com/custom"); // Add custom namespaces if needed
+
                 // Extract channel-level information
                 var channelNode = xmlDoc.SelectSingleNode("//channel");
                 if (channelNode == null)
@@ -81,24 +88,45 @@ namespace Jammer
                 {
                     foreach (XmlNode itemNode in itemNodes)
                     {
-                        var itemTitle = itemNode.SelectSingleNode("title")?.InnerText ?? "Unknown Title";
-                        var itemDescription = itemNode.SelectSingleNode("description")?.InnerText ?? "No Description";
-                        var itemPubDate = itemNode.SelectSingleNode("pubDate")?.InnerText ?? "Unknown Date";
-                        var itemAuthor = itemNode.SelectSingleNode("author")?.InnerText ?? "Unknown Author";
-
-                        // Find the media file
-                        var itemLink = itemNode.SelectSingleNode("enclosure")?.Attributes["url"]?.Value ??
-                                       itemNode.SelectSingleNode("media:content")?.Attributes["url"]?.Value ??
-                                       itemNode.SelectSingleNode("link")?.InnerText ?? "Unknown Link";
-
-                        items.Add(new IndividualRssData
+                        try
                         {
-                            Title = itemTitle,
-                            Link = itemLink,
-                            Description = itemDescription,
-                            PubDate = itemPubDate,
-                            Author = itemAuthor
-                        });
+                            var itemTitle = itemNode.SelectSingleNode("title")?.InnerText ?? "Unknown Title";
+                            var itemDescription = itemNode.SelectSingleNode("description")?.InnerText ?? "No Description";
+                            var itemPubDate = itemNode.SelectSingleNode("pubDate")?.InnerText ?? "Unknown Date";
+                            var itemAuthor = itemNode.SelectSingleNode("author")?.InnerText ??
+                                             itemNode.SelectSingleNode("dc:creator", namespaceManager)?.InnerText ??
+                                             itemNode.SelectSingleNode("media:credit", namespaceManager)?.InnerText ??
+                                             itemNode.SelectSingleNode("custom:author", namespaceManager)?.InnerText ??
+                                             itemNode.SelectSingleNode("itunes:author", namespaceManager)?.InnerText ??
+                                             null;
+
+                            // Look for the media file in multiple possible locations
+                            var itemLink = itemNode.SelectSingleNode("enclosure")?.Attributes["url"]?.Value ?? // Standard RSS enclosure
+                                           itemNode.SelectSingleNode("media:content", namespaceManager)?.Attributes["url"]?.Value ?? // Media RSS
+                                           itemNode.SelectSingleNode("media:group/media:content", namespaceManager)?.Attributes["url"]?.Value ?? // Media RSS group
+                                           itemNode.SelectSingleNode("link")?.InnerText ?? // Fallback to <link>
+                                           itemNode.SelectSingleNode("guid")?.InnerText ?? // GUID as a fallback
+                                           "Unknown Link";
+
+
+                            if (itemAuthor == null)
+                            {
+                                itemAuthor = author;
+                            }
+
+                            items.Add(new IndividualRssData
+                            {
+                                Title = itemTitle,
+                                Link = itemLink,
+                                Description = itemDescription,
+                                PubDate = itemPubDate,
+                                Author = itemAuthor
+                            });
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error($"Failed to extract item data: {ex.Message}");
+                        }
                     }
                 }
 
